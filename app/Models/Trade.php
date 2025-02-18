7c17bd6f0aed89e42ef07e375743b76fde258bc5
Jamie.vg | Easyzly <117265805+Easyzly@users.noreply.github.com>
2025-02-18 14:05:20 +0100

    +trade backend

---
+++ app/Models/Trade.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Trade extends Model
+{
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'item_id',
+        'quantity',
+        'accepted',
+    ];
+
+    public function sender(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+
+    public function item(): BelongsTo
+    {
+        return $this->belongsTo(Item::class);
+    }
+}
