b7d14cbafe7a3639222f1a8143fbdd8c5ca56e32
Jamie.vg | Easyzly <117265805+Easyzly@users.noreply.github.com>
2025-02-25 11:57:45 +0100

    !error fix for breadcrumbs - trades create

--- resources/views/trades/create.blade.php
+++ resources/views/trades/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ Breadcrumbs::render('trades.create') }}
+            {{ Breadcrumbs::render('trades.create', $item, $user) }}
         </h2>
     </x-slot>
 
